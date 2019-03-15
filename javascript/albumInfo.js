b0602b5488b81236cd571709faaa13dc709a03c7
hanlin <136655493@qq.com>
2019-03-15 14:52:42 +0800

    v0.3.15

---
+++ javascript/albumInfo.js
@@ -0,0 +1,153 @@
+
+//相册数据
+
+var albumInfo = [{
+    "src": [
+        "image/changbaishan/p1.jpg",
+        "image/changbaishan/p2.jpg",
+        "image/changbaishan/p3.jpg",
+        "image/changbaishan/p4.jpg",
+        "image/changbaishan/p5.jpg",
+        "image/changbaishan/p6.jpg",
+        "image/changbaishan/p7.jpg",
+        "image/changbaishan/p8.jpg"
+    ],
+    "position": "吉林·长白山",
+    "tab": ["雪山", "天池"],
+    "author": {
+        "id": "18810854185",
+        "name": "Lena",
+        "head": "image/touxiang.jpg"
+    },
+    "uploadTime": "2019/03/01 21:26",
+    "takePictureTime": "2018/10/05"
+}, {
+    "src": [
+        "image/bali/p1.jpg",
+        "image/bali/p2.jpg",
+        "image/bali/p3.jpg",
+        "image/bali/p4.jpg",
+        "image/bali/p5.jpg",
+        "image/bali/p6.jpg"
+    ],
+    "position": "印度尼西亚·巴厘岛",
+    "tab": ["海岛"],
+    "author": {
+        "id": "18810854186",
+        "name": "Lena2",
+        "head": "image/touxiang.jpg"
+    },
+    "uploadTime": "2019/03/01 20:42",
+    "takePictureTime": "2018/07/20"
+}, {
+    "src": [
+        "image/faded-lotus/p1.jpg",
+        "image/faded-lotus/p2.jpg",
+        "image/faded-lotus/p3.jpg",
+        "image/faded-lotus/p4.jpg",
+        "image/faded-lotus/p5.jpg",
+        "image/faded-lotus/p6.jpg"
+    ],
+    "position": "河北·避暑山庄",
+    "tab": ["荷花", "秋景"],
+    "author": {
+        "id": "18810854185",
+        "name": "Lena",
+        "head": "image/touxiang.jpg"
+    },
+    "uploadTime": "2019/03/01 15:44",
+    "takePictureTime": "2018/10/15"
+}, {
+    "src": [
+        "image/bird/p1.jpg",
+        "image/bird/p2.jpg",
+        "image/bird/p3.jpg",
+        "image/bird/p4.jpg",
+        "image/bird/p5.jpg",
+        "image/bird/p6.jpg"
+    ],
+    "position": "云南·滇池",
+    "tab": ["海鸥"],
+    "author": {
+        "id": "18810854185",
+        "name": "Lena",
+        "head": "image/touxiang.jpg"
+    },
+    "uploadTime": "2019/03/01 14:34",
+    "takePictureTime": "2018/03/02"
+}, {
+    "src": [
+        "image/guizhou/p1.jpg",
+        "image/guizhou/p2.jpg",
+        "image/guizhou/p3.jpg",
+        "image/guizhou/p4.jpg",
+        "image/guizhou/p5.jpg",
+        "image/guizhou/p6.jpg",
+        "image/guizhou/p7.jpg",
+        "image/guizhou/p8.jpg",
+        "image/guizhou/p9.jpg"
+    ],
+    "position": "贵州",
+    "tab": ["苗寨", "黄果树瀑布", "万峰林"],
+    "author": {
+        "id": "18810854185",
+        "name": "Lena",
+        "head": "image/touxiang.jpg"
+    },
+    "uploadTime": "2019/03/01 14:23",
+    "takePictureTime": "2018/02/24"
+}, {
+    "src": [
+        "image/weizhoudao/p1.jpg",
+        "image/weizhoudao/p2.jpg",
+        "image/weizhoudao/p3.jpg",
+        "image/weizhoudao/p4.jpg",
+        "image/weizhoudao/p5.jpg"
+    ],
+    "position": "广西·涠洲岛",
+    "tab": ["海岛", "落日"],
+    "author": {
+        "id": "18810854186",
+        "name": "Lena2",
+        "head": "image/touxiang.jpg"
+    },
+    "uploadTime": "2019/03/01 13:57",
+    "takePictureTime": "2018/02/20"
+}, {
+    "src": [
+        "image/yunnan/p1.jpg",
+        "image/yunnan/p2.jpg",
+        "image/yunnan/p3.jpg",
+        "image/yunnan/p4.jpg",
+        "image/yunnan/p5.jpg",
+        "image/yunnan/p6.jpg",
+        "image/yunnan/p7.jpg"
+    ],
+    "position": "云南·大理",
+    "tab": ["洱海", "苍山", "古镇"],
+    "author": {
+        "id": "18810854185",
+        "name": "Lena",
+        "head": "image/touxiang.jpg"
+    },
+    "uploadTime": "2019/03/01 13:10",
+    "takePictureTime": "2017/11/22"
+}, {
+    "src": [
+        "image/zhangjiajie/p1.jpg",
+        "image/zhangjiajie/p2.jpg",
+        "image/zhangjiajie/p3.jpg",
+        "image/zhangjiajie/p4.jpg",
+        "image/zhangjiajie/p5.jpg",
+        "image/zhangjiajie/p6.jpg"
+    ],
+    "position": "湖南·张家界",
+    "tab": ["森林公园", "金丝猴"],
+    "author": {
+        "id": "18810854185",
+        "name": "Lena",
+        "head": "image/touxiang.jpg"
+    },
+    "uploadTime": "2019/03/01 12:31",
+    "takePictureTime": "2017/07/10"
+}];
