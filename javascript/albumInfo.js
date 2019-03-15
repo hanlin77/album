9cefbe85f26df63d528332c0ebca5eb3ffa18e95
hanlin <136655493@qq.com>
2019-03-15 20:19:56 +0800

    choose photo

--- javascript/albumInfo.js
+++ javascript/albumInfo.js
@@ -7,10 +7,7 @@ var albumInfo = [{
         "image/changbaishan/p2.jpg",
         "image/changbaishan/p3.jpg",
         "image/changbaishan/p4.jpg",
-        "image/changbaishan/p5.jpg",
-        "image/changbaishan/p6.jpg",
-        "image/changbaishan/p7.jpg",
-        "image/changbaishan/p8.jpg"
+        "image/changbaishan/p5.jpg"
     ],
     "position": "吉林·长白山",
     "tab": ["雪山", "天池"],
@@ -27,8 +24,7 @@ var albumInfo = [{
         "image/bali/p2.jpg",
         "image/bali/p3.jpg",
         "image/bali/p4.jpg",
-        "image/bali/p5.jpg",
-        "image/bali/p6.jpg"
+        "image/bali/p5.jpg"
     ],
     "position": "印度尼西亚·巴厘岛",
     "tab": ["海岛"],
@@ -62,9 +58,7 @@ var albumInfo = [{
         "image/bird/p1.jpg",
         "image/bird/p2.jpg",
         "image/bird/p3.jpg",
-        "image/bird/p4.jpg"||"image/bird/p4.JPG",
-        "image/bird/p5.jpg",
-        "image/bird/p6.jpg"
+        "image/bird/p5.jpg"
     ],
     "position": "云南·滇池",
     "tab": ["海鸥"],
@@ -79,13 +73,10 @@ var albumInfo = [{
     "src": [
         "image/guizhou/p1.jpg",
         "image/guizhou/p2.jpg",
-        "image/guizhou/p3.jpg",
-        "image/guizhou/p4.jpg",
-        "image/guizhou/p5.jpg",
         "image/guizhou/p6.jpg",
         "image/guizhou/p7.jpg",
-        "image/guizhou/p8.jpg",
-        "image/guizhou/p9.jpg"
+        "image/guizhou/p9.jpg",
+        "image/guizhou/p10.jpg"
     ],
     "position": "贵州",
     "tab": ["苗寨", "黄果树瀑布", "万峰林"],
@@ -101,8 +92,7 @@ var albumInfo = [{
         "image/weizhoudao/p1.jpg",
         "image/weizhoudao/p2.jpg",
         "image/weizhoudao/p3.jpg",
-        "image/weizhoudao/p4.jpg",
-        "image/weizhoudao/p5.jpg"
+        "image/weizhoudao/p4.jpg"
     ],
     "position": "广西·涠洲岛",
     "tab": ["海岛", "落日"],
@@ -118,10 +108,7 @@ var albumInfo = [{
         "image/yunnan/p1.jpg",
         "image/yunnan/p2.jpg",
         "image/yunnan/p3.jpg",
-        "image/yunnan/p4.jpg",
-        "image/yunnan/p5.jpg",
-        "image/yunnan/p6.jpg",
-        "image/yunnan/p7.jpg"
+        "image/yunnan/p4.jpg"
     ],
     "position": "云南·大理",
     "tab": ["洱海", "苍山", "古镇"],
@@ -132,24 +119,6 @@ var albumInfo = [{
     },
     "uploadTime": "2019/03/01 13:10",
     "takePictureTime": "2017/11/22"
-}, {
-    "src": [
-        "image/zhangjiajie/p1.jpg",
-        "image/zhangjiajie/p2.jpg",
-        "image/zhangjiajie/p3.jpg",
-        "image/zhangjiajie/p4.jpg",
-        "image/zhangjiajie/p5.jpg",
-        "image/zhangjiajie/p6.jpg"
-    ],
-    "position": "湖南·张家界",
-    "tab": ["森林公园", "金丝猴"],
-    "author": {
-        "id": "18810854185",
-        "name": "Lena",
-        "head": "image/touxiang.jpg"
-    },
-    "uploadTime": "2019/03/01 12:31",
-    "takePictureTime": "2017/07/10"
 }, {
     "src": [
         "image/flowers/p1.jpg",
@@ -166,20 +135,4 @@ var albumInfo = [{
     },
     "uploadTime": "2019/03/01 12:02",
     "takePictureTime": "2017/05/05"
-}, {
-    "src": [
-        "image/snow/p1.jpg",
-        "image/snow/p2.jpg",
-        "image/snow/p3.jpg",
-        "image/snow/p4.jpg"
-    ],
-    "position": "江苏·苏州",
-    "tab": ["雪景"],
-    "author": {
-        "id": "18810854185",
-        "name": "Lena",
-        "head": "image/touxiang.jpg"
-    },
-    "uploadTime": "2019/03/01 11:30",
-    "takePictureTime": "2018/12/24"
 }];
